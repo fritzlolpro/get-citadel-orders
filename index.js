dad571a006a9e43ac51d96b62eda5b835d3d07a4
krushenina <ushenak@mail.ru>
2018-10-31 22:34:13 +0300

    remove co

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Order = require('./ordersSucker')
+bconst Order = require('./ordersSucker')
 const { getNewestDBName, currentDate } = require('./ordersSucker')
 const writeJsonToFile = require('./fileWriter')
 
@@ -14,77 +14,46 @@ const dbUrl = 'mongodb://localhost:27017/market';
 const orderComposer = (order) => {
 
 }
+const goonDB = mongoose.model(`${getNewestDBName('Delve')(currentDate)}`)
+const forgeDB = mongoose.model(`${getNewestDBName('Forge')(currentDate)}`)
+
 
 const comparePrices = async () => {
   // todo remove co, build actual name with function, use mongoose to suck
-  co(function* () {
-    const db = yield MongoClient.connect(dbUrl)
-    // console.log(db)
-    const goonCollection = db.collection('Delve_07-22-18--02')
-    const forgeCollection = db.collection('Forge_07-22-18--02')
+  // co(function* () {
+  //   const db = yield MongoClient.connect(dbUrl)
+  //   // console.log(db)
+  //   const goonCollection = db.collection('Delve_07-22-18--02')
+  //   const forgeCollection = db.collection('Forge_07-22-18--02')
 
-    const goonOrderList = yield goonCollection.find().toArray()
+  //   const goonOrderList = yield goonCollection.find().toArray()
 
-    const goonTypeIds = goonOrderList.map(x => x.order['typeId'])
+  //   const goonTypeIds = goonOrderList.map(x => x.order['typeId'])
 
-    const query = {
-      'order.typeId': { $nin: goonTypeIds }
-    }
-    const forgeOrderList = yield forgeCollection.find(query).toArray()
+  //   const query = {
+  //     'order.typeId': { $nin: goonTypeIds }
+  //   }
+  //   const forgeOrderList = yield forgeCollection.find(query).toArray()
 
-    // const goodOrders =  forgeOrderList.map(forgeOrder => {
-    //   const id = forgeOrder['typeId']
-    //   const matchedGoonOrder = goonOrderList.filter(order => order['typeId'] === id)[0]
+   
 
-    //   const forgePrice = forgeOrder.order.id['price']
-    //   const delvePrice = matchedGoonOrder.order[id]['price']
+  //   const sliceUpper = (orderList) => {
+  //     orderList.map(order => {
 
-    //   if ((forgePrice * 1.2) <= delvePrice) {
-    //     return {
-    //       order: {
-    //         typeId: id,
-    //         fordge: {
-    //           ...forgeOrder.order[id]
-    //         },
-    //         delve: {
-    //           ...matchedGoonOrder.order[id]
-    //         }
-    //       }
-    //     }
-    //   }
-    // })
-    // console.log(goodOrders)
-
-    const sliceUpper = (orderList) => {
-      orderList.map(order => {
-
-      })
-    }
+  //     })
+  //   }
 
 
-    console.log(forgeOrderList.filter(x => x.order['40337']))
+  //   console.log(forgeOrderList.filter(x => x.order['40337']))
 
-    db.close()
-  }).catch(error => console.log(error.stack))
-  // const db = await mongo.MongoClient.connect(dbUrl)
-
-  // // MongoClient.connect(dbUrl, function(err, client) {
-  // //   assert.equal(null, err);
-  // //   // console.log("Connected successfully to database");
-  // //    const db = client.db('market');
-  //   // db.collection(name).insertMany([{...data}], function(err, r) {
-  //   //   assert.equal(null, err);
-  //   //   assert.equal(1, r.insertedCount);
-
-
-  //   // });
-  // // const goonOrdersCursor = db.collection('Goon_Capital').find()
-
-  // //   const goonOrderList = await goonOrdersCursor.next()
-  // //   console.log(goonOrderList)
-  // console.log(db)
-  //   db.close();
-  // })
+  //   db.close()
+  // }).catch(error => console.log(error.stack))
+  const goonOrderList = await goonDB.find().toArray()
+  const goonTypeIds = goonOrderList.map(x => x.order['typeId'])
+  const query = {
+        'order.typeId': { $nin: goonTypeIds }
+      }
+  const forgeOrderList = await forgeCollection.find(query).toArray()
 }
 
 
