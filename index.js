62d161d623b7e0f03414cf5f1a148c242c4d623e
fedor <livsnt123@gmail.com>
2018-11-23 19:25:18 +0100

    fuck formatting again, do all shit in O(n)

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-require('dotenv').config();
-const Order = require('./ordersSucker')
-const writeJsonToFile = require('./fileWriter')
-const { goonDB, forgeDB } = require('./dbConnector')
+require("dotenv").config();
+const Order = require("./ordersSucker");
+const writeJsonToFile = require("./fileWriter");
+const { goonDB, forgeDB } = require("./dbConnector");
 
+const comparePrices = async () => {
+	const goonOrders = Array.from(await goonDB.find({}));
+	const goonOrderIdList = Array.from(
+		new Set(goonOrders.map(x => x.order.typeId))
+	);
 
+	const query = {
+		"order.typeId": { $nin: goonOrderIdList }
+	};
 
+	const forgeOrders = await forgeDB.find(query);
 
+	// 1. group all orders with same type ID
 
+	const orderMerger = orders => {
+		// !!! FUCK PIPELINES, DONT NEED THEM AS I CAN O(n) THIS SHIT
+		let separatedOrders = {};
+		let existingItemsKeys = [];
 
-const comparePrices = async() => {
+		orders.forEach(item => {
+			const order = item.order;
 
-    const goonOrders = Array.from(await goonDB.find({}))
-    const goonOrderIdList = Array.from(new Set(goonOrders.map(x => x.order.typeId)))
+			if (existingItemsKeys.includes(order.typeId)) {
+				separatedOrders[order.typeId][order.body.isBuy ? "buy" : "sell"]
+					? separatedOrders[order.typeId][order.body.isBuy ? "buy" : "sell"][
+							"price"
+					  ].push(order.body.price)
+					: (separatedOrders[order.typeId][
+							order.body.isBuy ? "buy" : "sell"
+					  ] = { price: [order.body.price] });
+			} else {
+				separatedOrders[order.typeId] = {};
+				separatedOrders[order.typeId][order.body.isBuy ? "buy" : "sell"] = {
+					price: [order.body.price]
+				};
+			}
+			existingItemsKeys.push(order.typeId);
+		});
+		// console.log(separatedOrders);
+		return separatedOrders;
+	};
 
-    const query = {
-        'order.typeId': { $nin: goonOrderIdList }
-    }
+	const cutTooSmall = prices =>
+		prices
+			.sort((a, b) => a - b)
+			.map((price, i, arr) => {
+				if (price * 10 < arr[arr.length - 1]) {
+					arr[arr.length - 1] = "";
+				}
+				return price;
+			});
 
-    const forgeOrders = await forgeDB.find(query)
-    const forgeOrderIdList = Array.from(new Set(forgeOrders.map(x => x.order.typeId)))
-
-    // 1. group all orders with same type ID
-
-    const orderMerger = (idList, orders) => {
-        // !!! FUCK PIPELINES, DONT NEED THEM AS I CAN O(n) THIS SHIT
-        let separatedOrders = {}
-        let existingItemsKeys = []
-
-        orders.forEach(item => {
-            const order = item.order
-
-
-            if (existingItemsKeys.includes(order.typeId)) {
-                console.log('hui')
-            } else {
-                separatedOrders[order.typeId] = {}
-                separatedOrders[order.typeId][order.body.isBuy ? 'buy' : 'sell'] = {
-                    price: [order.body.price]
-                }
-                existingItemsKeys.push(order.typeId)
-            }
-        })
-        console.log(separatedOrders)
-            // return separatedOrders
-    }
-
-    const cutTooSmall = prices => prices.sort((a, b) => a - b).map((price, i, arr) => {
-        if (price * 10 < arr[arr.length - 1]) {
-            arr[arr.length - 1] = ''
-        }
-        return price
-    })
-
-    orderMerger(goonOrderIdList, goonOrders)
-        /*
+	orderMerger(goonOrders);
+	/*
           order: {
             typeID: xxxx,
             price: [all prices],
@@ -61,25 +64,22 @@ const comparePrices = async() => {
             ...other fields
           }
         */
-        // 2. in every order balance prices
-}
-
+	// 2. in every order balance prices
+};
 
 const GoonHome = {
-    placeId: 1022734985679,
-    name: 'Delve'
-}
+	placeId: 1022734985679,
+	name: "Delve"
+};
 
 const Forge = {
-    placeId: 10000002,
-    name: 'Forge',
-    queryModificator: '/orders'
-}
+	placeId: 10000002,
+	name: "Forge",
+	queryModificator: "/orders"
+};
 
-
-const structuresCallUrl = 'https://esi.tech.ccp.is/latest/markets/structures/'
-const regionCallUrl = 'https://esi.evetech.net/latest/markets/'
-
+const structuresCallUrl = "https://esi.tech.ccp.is/latest/markets/structures/";
+const regionCallUrl = "https://esi.evetech.net/latest/markets/";
 
 // const goonOrders = new Order()
 // goonOrders.structure = GoonHome;
@@ -99,4 +99,4 @@ const regionCallUrl = 'https://esi.evetech.net/latest/markets/'
 
 //     })
 
-comparePrices()
+comparePrices();
